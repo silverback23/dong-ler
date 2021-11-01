4469bcce0db85a12b784691b20c0ca741b6aaf37
silverback23 <cahid23@hotmail.com>
2021-11-01 14:11:28 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayac=int.Parse(Console.ReadLine());
+            for (int i = 0; i < sayac; i++)
+            {
+                 if(i%2==1)
+                 Console.Write(i);
+            }
+            int tekToplam=0;
+            int cifttoplam=0;
+
+            for (int i = 1; i < 1000; i++)
+            {
+                if(i%2==1)
+                    tekToplam+=i;
+                else
+                cifttoplam +=i;
+            }
+            Console.WriteLine("Tek toplam"+tekToplam);
+            Console.WriteLine("çift toplam"+cifttoplam);
+
+            //break,continue
+             for(int i = 0; i < 10; i++)
+            {
+            
+            if(i==4)
+              break;
+            Console.Write(i);
+
+            }
+            for(int i = 0; i < 10; i++)
+            {
+            
+            if(i==4)
+              continue;
+            Console.Write(i);
+
+            }
+       
+     }
+
+       
+       
+    
+        
+    }
+}
